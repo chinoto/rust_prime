f2a75a47f72ba7486d6df0dabb4e4ce441c478b1
Damian J Pound <damian@thebestisp.com>
2025-11-30 13:51:28 -0600

    prime_mt_rayon: Replace comments

--- src/bin/prime_mt_rayon.rs
+++ src/bin/prime_mt_rayon.rs
@@ -8,14 +8,9 @@ fn main() {
 
     while test < test_halt {
         let test_limit = primes.last().unwrap().pow(2).min(test_halt);
-        // Can't push into primes list while reading it because that might cause a reallocation,
-        // thus breaking any readers. Also, the borrow checker doesn't like it...
-
-        // The copy vector's allocation can't be reused because the source vector will always be
-        // bigger and require a reallocation.
-
-        // TODO: Potential solution is to pad the primes vector with 0s and use .split_at_mut() to
-        // read the existing primes in the first slice and write the new primes in the second slice.
+        // Copy the known primes, rather than collecting new primes and appending
+        // because known primes length is likely less than the new primes length.
+        // k+(k+n) < n+(k+n)
         let primes_copy = primes.clone();
         primes.par_extend(
             // rayon doesn't support `a..=b`, `a..b+1` is equivalent in this case.
